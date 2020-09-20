db69d0fd0ad47369fea0008096114719d5cf4af0
GuiJordao <guilherme@netpark.com.br>
2020-09-20 15:06:52 -0300

    feat(events) engine:
    Added simple event engine trigger

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -130,6 +130,7 @@ var User =
 function () {
   function User(data) {
     this.data = data;
+    this.events = {};
   }
 
   User.prototype.get = function (propName) {
@@ -138,6 +139,24 @@ function () {
 
   User.prototype.set = function (update) {
     Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(function (callback) {
+      callback();
+    });
   };
 
   return User;
@@ -157,11 +176,16 @@ var user = new user_1.User({
   name: "Gui",
   age: 26
 });
-user.set({
-  name: "new name"
+user.on("change", function () {
+  console.log("change 1");
 });
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.on("change", function () {
+  console.log("change 2");
+});
+user.on("save", function () {
+  console.log("save 1");
+});
+user.trigger("save");
 },{"./models/user":"src/models/user.ts"}],"../../../../.nvm/versions/node/v10.16.2/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -190,7 +214,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50037" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49900" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import { User } from "./models/user";
 
 const user = new User({ name: "Gui", age: 26 });
 
-user.set({ name: "new name" });
+user.on("change", () => {
+  console.log("change 1");
+});
+user.on("change", () => {
+  console.log("change 2");
+});
+user.on("save", () => {
+  console.log("save 1");
+});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.trigger("save");
