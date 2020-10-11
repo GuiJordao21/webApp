a2a294272892463d6523c207990af1ac3b2ceebe
GuiJordao <guilherme@netpark.com.br>
2020-10-11 15:27:35 -0300

    feat(generics) advanced generics contraints:
    Wotking with generics for methods

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,21 +1,17 @@
 import { Eventing } from "./Eventing";
-
+import { Sync } from "./Sync"
+ 
 export interface UserProps {
   age?: number;
   id?: number;
   name?: string;
 }
 
+const rootUrl = "http://localhost:3000/user"
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  
 }
