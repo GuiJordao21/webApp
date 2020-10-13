577eacd3d045f5efd826c68f3ae2a23d4ad9249a
GuiJordao <guilherme@netpark.com.br>
2020-10-13 19:07:56 -0300

    adding more methods to user

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1, age: 33, name: "Gui" });
-
-user.events.on("change", () => {
-  console.log("test");
-});
-
-user.events.trigger("change");
+const user = new User({id: 3})
+console.log(user)
+user.fetch()
+user.on("change", ()=> {
+  console.log(user);
+})
